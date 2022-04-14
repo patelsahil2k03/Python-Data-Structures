3c795629af23c7d90db27d2780eedd805c315b7c
patelsahil2k03 <patelsahil2k03@gmail.com>
2022-04-14 13:27:21 +0530

    maximum amount of water a container can store

---
+++ Python practical assignment/pipprogassignprac3.py
@@ -0,0 +1,16 @@
+# print('''Code written by
+# SAHIL PATEL
+# 20CE101''')
+
+def maxArea(A, Len):
+    area = 0
+    for i in range(Len):
+        for j in range(i + 1, Len):
+            area = max(area, min(A[j], A[i]) * (j - i))
+    return area
+
+
+a = [int(n) for n in input("Enter an array: ").split()]
+
+len1 = len(a)
+print(maxArea(a, len1))
